0dac8ad4ff3acd778713bd05cd68aa7ea61e5c1e
vedioislamic <vedioislamic558@gmail.com>
2025-08-03 20:36:55 -0700

    Added root route to main.py

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ def read_root():
     return {"message": "API working fine"}
 
 # Download example file from Hugging Face
-model_path = hf_hub_download(
-    repo_id="stabilityai/stable-diffusion-2-1",
-    filename="model_index.json"
+model_path = hf_hub_download
+repo_id="stabilityai/stable-diffusion-2-1",
+filename="model_index.json"
